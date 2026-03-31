b73655b5f0364f76398fe02549569d57023e4c71
Rafael-Borges318 <rb2080500@gmail.com>
2026-03-31 16:19:33 -0300

    estrturação melhorada back-end

--- back-end/src/routes/servicos.js
+++ back-end/src/routes/servicos.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { supabase } from "../supabaseClient.js";
+import { supabase } from "../config/supabaseClient.js";
 import { adminAuth } from "../middleware/adminAuth.js";
 
 const router = express.Router();
